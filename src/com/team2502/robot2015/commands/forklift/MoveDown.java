26d0e8471a1606ff22a1616a0fc65a0e6e71ac6e
supersensai <cjelliott1297@gmail.com>
2015-02-05 17:03:17 -0600

    added MoveDown to forklift commands

--- src/com/team2502/robot2015/commands/forklift/MoveDown.java
+++ src/com/team2502/robot2015/commands/forklift/MoveDown.java
@@ -7,30 +7,41 @@ import edu.wpi.first.wpilibj.command.Command;
  */
 public class MoveDown extends Command {
 
+	private double distance;
+	private double speed;
+	private double startDistance;
+	private double Forklift fl = Robot.forklift;
     public MoveDown() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
+    	requires(Robot.forklift);
+    	this.distance = distance;
+    	this.speed = speed;
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	this.startDistance = fl.getHeight();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	fl.move(speed);
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return fl.getHeight() - distance > startDistance;
     }
 
     // Called once after isFinished returns true
     protected void end() {
+    	fl.stopAll();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	end();
     }
 }
