c63f821deea926bf03a629ddc4265afc3f77de81
BinEP <bradystoffel@gmail.com>
2015-02-23 20:01:14 -0600

    TurnDegrees written

--- src/com/team2502/robot2015/commands/drive/TurnDegrees.java
+++ src/com/team2502/robot2015/commands/drive/TurnDegrees.java
@@ -1,7 +1,10 @@
 package com.team2502.robot2015.commands.drive;
 
+import java.util.ArrayList;
+
 import com.team2502.robot2015.Robot;
 import com.team2502.robot2015.subsystems.DriveTrain;
+import com.team2502.robot2015.subsystems.DriveTrain.Motors;
 
 import edu.wpi.first.wpilibj.command.Command;
 
@@ -12,25 +15,57 @@ public class TurnDegrees extends Command {
 
 	private double degrees;
 	private double speed;
+	private double distance;
+	private double left;
+	private double right;
 	private DriveTrain dt = Robot.driveTrain;
+	private ArrayList<Double> lastEncoderValues = new ArrayList<Double>();
+	private double movedDistance = 0;
+//	private double startTime;
 	
-    public TurnDegrees() {
+    public TurnDegrees(double degrees, double speed) {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
     	requires(Robot.driveTrain);
+    	this.speed = speed;
+    	this.degrees = degrees;
+    	this.distance = degrees * (DriveTrain.WHEEL_DIAMETER * Math.PI);
+    	left = (degrees > 0) ? 1 : -1;
+    	right = -left;
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	for (Motors m : Motors.values()) {
+    		lastEncoderValues.add(dt.getEncoderValue(m));
+    	}
+//    	startTime = System.currentTimeMillis();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	dt.moveMainDrive(left, right);
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+    	ArrayList<Double> encoderValues = new ArrayList<Double>();
+    	for (Motors m : Motors.values()) {
+    		encoderValues.add(dt.getEncoderValue(m));
+    	}
+    	
+    	double diff = encoderValues.get(0) - lastEncoderValues.get(0);
+    	double smallestDiff = encoderValues.get(0) - lastEncoderValues.get(0);
+    	for (int i = 1; i < encoderValues.size(); i++) {
+    		if (i != 2 && i != 5) {
+    			diff = encoderValues.get(i) - lastEncoderValues.get(i);
+    			if (Math.abs(diff) < Math.abs(smallestDiff)) smallestDiff = diff;
+    		}
+    	}
+    	movedDistance += smallestDiff;
+    	lastEncoderValues = encoderValues;
+    	
+    	return ((DriveTrain.WHEEL_DIAMETER * Math.PI) * movedDistance) - distance > 0;
     }
 
     // Called once after isFinished returns true
