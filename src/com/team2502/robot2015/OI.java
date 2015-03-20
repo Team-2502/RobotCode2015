874b60eaf493e3c7f1eca731aba6e68740c70d1b
vandench <18maasd@gmail.com>
2015-03-20 18:30:43 -0500

    Removed togglefork button.

--- src/com/team2502/robot2015/OI.java
+++ src/com/team2502/robot2015/OI.java
@@ -3,12 +3,8 @@ package com.team2502.robot2015;
 import com.team2502.robot2015.commands.forklift.SetLeftForced;
 import com.team2502.robot2015.commands.forklift.SetRightForced;
 import com.team2502.robot2015.commands.forklift.SpinIntake;
-import com.team2502.robot2015.commands.forklift.ToggleDirection;
 import com.team2502.robot2015.commands.forklift.ToggleForklift;
 //import com.team2502.robot2015.commands.vision.ToggleCamera;
-
-
-
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.Button;
@@ -19,8 +15,9 @@ import edu.wpi.first.wpilibj.buttons.JoystickButton;
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
-	//// CREATING BUTTONS
-	// One type of button is a joystick button which is any button on a joystick.
+	// // CREATING BUTTONS
+	// One type of button is a joystick button which is any button on a
+	// joystick.
 	// You create one by telling it which joystick it's on and which button
 	// number it is.
 	// Joystick stick = new Joystick(port);
@@ -30,7 +27,7 @@ public class OI {
 	// by subclassing Button you can create custom triggers and bind those to
 	// commands the same as any other Button.
 
-	//// TRIGGERING COMMANDS WITH BUTTONS
+	// // TRIGGERING COMMANDS WITH BUTTONS
 	// Once you have a button, it's trivial to bind it to a button in one of
 	// three ways:
 
@@ -42,14 +39,14 @@ public class OI {
 	// the button is released.
 	// button.whileHeld(new ExampleCommand());
 
-	// Start the command when the button is released  and let it run the command
+	// Start the command when the button is released and let it run the command
 	// until it is finished as determined by it's isFinished method.
 	// button.whenReleased(new ExampleCommand());
 
 	private static Joystick driveStick;
 	private static Joystick liftStick;
 	private static Button forkliftButton;
-	private static Button forkliftDirection;
+	// private static Button forkliftDirection;
 	private static Button toggleCameraButton;
 	private static Button forceLeft;
 	private static Button forceRight;
@@ -60,25 +57,25 @@ public class OI {
 	public OI() {
 		driveStick = new Joystick(RobotMap.RIGHT_JOYSTICK);
 		toggleCameraButton = new JoystickButton(driveStick, 2);
-//		toggleCameraButton.whenPressed(new ToggleCamera());
-		
+		// toggleCameraButton.whenPressed(new ToggleCamera());
+
 		liftStick = new Joystick(RobotMap.LEFT_JOYSTICK);
 		forkliftButton = new JoystickButton(liftStick, 1);
 		forkliftButton.whenPressed(new ToggleForklift());
-		
+
 		activeIntakeButtonIn = new JoystickButton(driveStick, 1);
 		activeIntakeButtonIn.whileHeld(new SpinIntake(true));
-		
+
 		activeIntakeButtonOut = new JoystickButton(driveStick, 2);
 		activeIntakeButtonOut.whileHeld(new SpinIntake(false));
-		
-		forkliftDirection = new JoystickButton(liftStick, 3);
-		forkliftDirection.whenPressed(new ToggleDirection());
-		
+
+		// forkliftDirection = new JoystickButton(liftStick, 3);
+		// forkliftDirection.whenPressed(new ToggleDirection());
+
 		forceLeft = new JoystickButton(liftStick, 4);
 		forceLeft.whenPressed(new SetLeftForced(true));
 		forceLeft.whenReleased(new SetLeftForced(false));
-		
+
 		forceRight = new JoystickButton(liftStick, 5);
 		forceRight.whenPressed(new SetRightForced(true));
 		forceRight.whenReleased(new SetRightForced(false));
@@ -87,11 +84,10 @@ public class OI {
 	public static Joystick getDriveStick() {
 		return driveStick;
 	}
-	
+
 	public static Joystick getLiftStick() {
-		
+
 		return liftStick;
-		
+
 	}
 }
-
--- src/com/team2502/robot2015/Robot.java
+++ src/com/team2502/robot2015/Robot.java
@@ -1,9 +1,14 @@
-
 package com.team2502.robot2015;
 
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
+
+import com.team2502.robot2015.commands.autonomous.Move;
+import com.team2502.robot2015.commands.autonomous.Pickup;
+import com.team2502.robot2015.subsystems.DriveTrain;
+import com.team2502.robot2015.subsystems.Forklift;
+import com.team2502.robot2015.subsystems.ScorpionTail;
 
 import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.IterativeRobot;
@@ -14,15 +19,6 @@ import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-import com.team2502.robot2015.commands.ExampleCommand;
-import com.team2502.robot2015.commands.autonomous.Move;
-import com.team2502.robot2015.commands.autonomous.Pickup;
-import com.team2502.robot2015.commands.autonomous.RecycleBinPickup;
-import com.team2502.robot2015.subsystems.DriveTrain;
-import com.team2502.robot2015.subsystems.ExampleSubsystem;
-import com.team2502.robot2015.subsystems.Forklift;
-import com.team2502.robot2015.subsystems.ScorpionTail;
-
 /**
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the IterativeRobot
@@ -31,12 +27,18 @@ import com.team2502.robot2015.subsystems.ScorpionTail;
  * directory.
  */
 public class Robot extends IterativeRobot {
-	
+
+	// Modes for the Autonomous period
+	// NOTHING : Do nothing for this part of autonomous
+	// MOVE : Move for a certain period of time around the field
+	// PICKUP_MOVE : Pickup an 'Item' and move for a certain period of time
+	// around the field
 	enum AutoModes {
 		NOTHING, MOVE, PICKUP_MOVE;
 	}
 
-//	public static final ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
+	// public static final ExampleSubsystem exampleSubsystem = new
+	// ExampleSubsystem();
 	public static final DriveTrain driveTrain = DriveTrain.getInstance();
 	public static OI oi;
 	public static final Forklift forklift = new Forklift();
@@ -44,88 +46,98 @@ public class Robot extends IterativeRobot {
 	public static final PowerDistributionPanel PDP = new PowerDistributionPanel();
 
 	SendableChooser autoChooser = new SendableChooser();
-    Command autonomousCommand;
+	Command autonomousCommand;
 
-    /**
-     * This function is run when the robot is first started up and should be
-     * used for any initialization code.
-     */
-    public void robotInit() {
+	/**
+	 * This function is run when the robot is first started up and should be
+	 * used for any initialization code.
+	 */
+	public void robotInit() {
 		oi = new OI();
 		CameraServer.getInstance().startAutomaticCapture("cam0");
-        // instantiate the command used for the autonomous period
-//        autonomousCommand = new ExampleCommand();
-//		autonomousCommand = new RecycleBinPickup();
-		autoChooser.addObject("Pickup and Move Backwards", AutoModes.PICKUP_MOVE);
+		// instantiate the command used for the autonomous period
+		// autonomousCommand = new ExampleCommand();
+		// autonomousCommand = new RecycleBinPickup();
+		autoChooser.addObject("Pickup and Move Backwards",
+				AutoModes.PICKUP_MOVE);
 		autoChooser.addObject("Move Backwards", AutoModes.MOVE);
 		autoChooser.addDefault("Do Nothing", AutoModes.NOTHING);
 		SmartDashboard.putData("Auto Mode", autoChooser);
-    }
-	
+	}
+
 	public void disabledPeriodic() {
+		// Gets the next instance in the Scheduler (should be the disabled
+		// command)
 		Scheduler.getInstance().run();
 	}
 
-    public void autonomousInit() {
-    	switch ((AutoModes) autoChooser.getSelected()) {
-    	case PICKUP_MOVE:
-    		autonomousCommand = new Pickup();
-    		break;
-    	case MOVE:
-    		autonomousCommand = new Move();
-    		break;
-    	case NOTHING:
-    		autonomousCommand = null;
-    		break;
-    	}
-        // schedule the autonomous command (example)
-        if (autonomousCommand != null) autonomousCommand.start();
-    }
+	public void autonomousInit() {
+		switch ((AutoModes) autoChooser.getSelected()) {
+		case PICKUP_MOVE:
+			autonomousCommand = new Pickup();
+			break;
+		case MOVE:
+			autonomousCommand = new Move();
+			break;
+		case NOTHING:
+			autonomousCommand = null;
+			break;
+		}
+		// schedule the autonomous command (example)
+		if (autonomousCommand != null)
+			autonomousCommand.start();
+	}
 
-    /**
-     * This function is called periodically during autonomous
-     */
-    public void autonomousPeriodic() {
-        Scheduler.getInstance().run();
-    }
+	/**
+	 * This function is called periodically during autonomous
+	 */
+	public void autonomousPeriodic() {
+		// Gets the next instance in the Scheduler (should be the next command
+		// in autonomous)
+		Scheduler.getInstance().run();
+	}
 
-    public void teleopInit() {
+	public void teleopInit() {
 		// This makes sure that the autonomous stops running when
-        // teleop starts running. If you want the autonomous to 
-        // continue until interrupted by another command, remove
-        // this line or comment it out.
-        if (autonomousCommand != null) autonomousCommand.cancel();
-        try {
+		// teleop starts running. If you want the autonomous to
+		// continue until interrupted by another command, remove
+		// this line or comment it out.
+		if (autonomousCommand != null)
+			autonomousCommand.cancel();
+		try {
 			PrintWriter writer = new PrintWriter("currentLog.log", "UTF-8");
 		} catch (FileNotFoundException | UnsupportedEncodingException e) {
 			e.printStackTrace();
 		}
-    }
+	}
 
-    /**
-     * This function is called when the disabled button is hit.
-     * You can use it to reset subsystems before shutting down.
-     */
-    public void disabledInit(){
+	/**
+	 * This function is called when the disabled button is hit. You can use it
+	 * to reset subsystems before shutting down.
+	 */
+	public void disabledInit() {
+		// Nothing is here since nothing needs to be reset.
+	}
 
-    }
+	/**
+	 * This function is called periodically during operator control
+	 */
+	public void teleopPeriodic() {
+		Scheduler.getInstance().run();
+		forklift.updateForkliftDashboard();
+		driveTrain.updateDriveDashboard();
+		// writer.println(System.currentTimeMillis() + " FrontRight: " +
+		// PDP.getCurrent(12));
+		// writer.println(System.currentTimeMillis() + " FrontLeft: " +
+		// PDP.getCurrent(14));
+		SmartDashboard.putData("PDP", PDP);
+	}
 
-    /**
-     * This function is called periodically during operator control
-     */
-    public void teleopPeriodic() {
-        Scheduler.getInstance().run();
-        forklift.updateForkliftDashboard();
-        driveTrain.updateDriveDashboard();
-//        writer.println(System.currentTimeMillis() + " FrontRight: " + PDP.getCurrent(12));
-  //      writer.println(System.currentTimeMillis() + " FrontLeft: " + PDP.getCurrent(14));
-        SmartDashboard.putData("PDP", PDP);
-    }
-    
-    /**
-     * This function is called periodically during test mode
-     */
-    public void testPeriodic() {
-        LiveWindow.run();
-    }
+	/**
+	 * This function is called periodically during test mode
+	 */
+	// Makes sure everything is running properly
+	public void testPeriodic() {
+		LiveWindow.run();
+	}
 }
