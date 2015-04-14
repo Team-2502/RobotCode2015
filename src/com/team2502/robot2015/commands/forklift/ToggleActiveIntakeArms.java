aedf4487f9b51e867ff73eedeb8bd9c96201572e
BinEP <bradystoffel@gmail.com>
2015-04-14 09:54:38 -0500

    Renamed method to match other things

--- src/com/team2502/robot2015/commands/forklift/ToggleActiveIntakeArms.java
+++ src/com/team2502/robot2015/commands/forklift/ToggleActiveIntakeArms.java
@@ -25,7 +25,7 @@ public class ToggleActiveIntakeArms extends Command {
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
     	
-    	fl.setState(!(fl.isOpenLeft() || fl.isOpenRight()));
+    	fl.setActiveIntakeState(!(fl.isOpenLeft() || fl.isOpenRight()));
     }
 
     // Make this return true when this Command no longer needs to run execute()
